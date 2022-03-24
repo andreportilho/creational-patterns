32d2319327e2ec758052e1b35c71c2967eca3a55
Andre Portilho <andre.portilhoo@gmail.com>
2022-03-24 17:56:57 -0300

    Builder pattern

--- src/main/java/br/com/cod3r/builder/meal/Client.java
+++ src/main/java/br/com/cod3r/builder/meal/Client.java
@@ -1,8 +1,20 @@
 package br.com.cod3r.builder.meal;
 
-public class Client {
-	
-	public static void main(String[] args) {
+import br.com.cod3r.builder.meal.builder.FastFoodMealBuilder;
+import br.com.cod3r.builder.meal.builder.p1Builder;
+import br.com.cod3r.builder.meal.director.MealDirector;
 
+public class Client {
+
+	public static void order(String name, FastFoodMealBuilder builder){
+		System.out.println("Ordering a " + name);
+		MealDirector director = new MealDirector(builder);
+		director.constructCombo();
+		System.out.println(director.getCombo());
+		System.out.println("------------------");
+	}
+
+	public static void main(String[] args) {
+		order("Burguer", new p1Builder());
 	}
 }
